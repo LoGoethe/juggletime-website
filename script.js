99d84093e2f76196efcf38464b9e662057c4b6b5
Logan Goethe <logoethe@gmail.com>
2025-02-27 14:51:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger");
-    const navLinks = document.getElementById("nav-links");
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
 
-    hamburger.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
+  hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
 });
